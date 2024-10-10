72c73d306d13dcdb387aad6cdceb6aca30415653
Jesse Braham <jesse@beta7.io>
2024-10-10 13:46:36 +0200

    Update dependencies

--- embassy/src/bin/firmware.rs
+++ embassy/src/bin/firmware.rs
@@ -6,36 +6,27 @@ use defmt_rtt as _;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
-use esp_hal::{
-    clock::ClockControl,
-    peripherals::Peripherals,
-    prelude::*,
-    rtc_cntl::Rtc,
-    system::SystemControl,
-    timer::timg::TimerGroup,
-};
+use esp_hal::{config::WatchdogStatus, prelude::*, rtc_cntl::Rtc, timer::timg::TimerGroup};
 
 #[main]
 async fn main(spawner: Spawner) {
-    let peripherals = Peripherals::take();
-    let system = SystemControl::new(peripherals.SYSTEM);
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
-
-    // Enable the RWDT watchdog timer:
-    let mut rtc = Rtc::new(peripherals.LPWR);
-    rtc.rwdt.set_timeout(2.secs());
-    rtc.rwdt.enable();
+    let peripherals = esp_hal::init({
+        let mut config = esp_hal::Config::default();
+        config.watchdog.rwdt = WatchdogStatus::Enabled(2.secs());
+        config
+    });
     info!("RWDT watchdog enabled!");
 
     // Initialize the SYSTIMER peripheral, and then Embassy:
-    let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks);
-    esp_hal_embassy::init(&clocks, timg0.timer0);
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_hal_embassy::init(timg0.timer0);
     info!("Embassy initialized!");
 
     // TODO: Spawn some tasks
     let _ = spawner;
 
     // Periodically feed the RWDT watchdog timer when our tasks are not running:
+    let mut rtc = Rtc::new(peripherals.LPWR);
     loop {
         rtc.rwdt.feed();
         Timer::after(Duration::from_secs(1)).await;
